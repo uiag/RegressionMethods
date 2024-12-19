82ae7e7b7fe2176dde955d0614ab4c631dfb0431
uiag <33594671+uiag@users.noreply.github.com>
2024-12-19 11:36:33 +0100

    Update mixedModel.R

--- mixedModel.R
+++ mixedModel.R
@@ -4,6 +4,7 @@ library(pROC)
 library(tidyr)
 library(MuMIn)
 library(lme4)
+library(lmerTest)
 set.seed(123)
 
 df <- read.csv("CoinToss/analyses/data-agg.csv",header=T)
@@ -20,6 +21,17 @@ df_long <- read.csv("CoinToss/analyses/data-long.csv",header=T)
 df_time_agg <- read.csv("CoinToss/analyses/df-time-agg.csv",header=T)
 df_time <- read.csv("CoinToss/analyses/df-time.csv",header=T)
 
+df_filtered <- df %>%
+  group_by(person, coin) %>%
+  filter(all(m >= 500)) %>%
+  ungroup()
+
+to_remove <- df %>%
+  group_by(person, coin) %>%
+  filter(any(m < 500)) %>%
+  summarise() %>%
+  ungroup()
+
 #Remove outliers first
 df <- df[df$person != "TianqiPeng", ]
 df <- df[df$person != "JanYang", ]
@@ -31,6 +43,17 @@ df <- subset(df, !(person == "FranziskaAssion" & coin == "1EUR"))
 df <- subset(df, !(person == "EJ" & coin == "50CZK"))
 df <- subset(df, !(person == "XiaoyiLin" & coin == "0.50EUR"))
 df <- subset(df, !(person == "JasonNak" & coin == "0.50EUR"))
+
+df_filtered <- df_filtered[df_filtered$person != "TianqiPeng", ]
+df_filtered <- df_filtered[df_filtered$person != "JanYang", ]
+df_filtered <- df_filtered[df_filtered$coin != "0.50SGD", ]
+df_filtered <- df_filtered[df_filtered$coin != "0.02EUR", ]
+df_filtered <- subset(df_filtered, !(person == "MagdaMatetovici" & coin == "1CAD"))
+df_filtered <- subset(df_filtered, !(person == "XiaochangZhao" & coin == "0.50SGD"))
+df_filtered <- subset(df_filtered, !(person == "FranziskaAssion" & coin == "1EUR"))
+df_filtered <- subset(df_filtered, !(person == "EJ" & coin == "50CZK"))
+df_filtered <- subset(df_filtered, !(person == "XiaoyiLin" & coin == "0.50EUR"))
+df_filtered <- subset(df_filtered, !(person == "JasonNak" & coin == "0.50EUR"))
 
 df_long <- df_long[df_long$person != "TianqiPeng", ]
 df_long <- df_long[df_long$person != "JanYang", ]
@@ -65,6 +88,10 @@ df_time_agg <- subset(df_time_agg, !(person == "EJ" & coin == "50CZK"))
 df_time_agg <- subset(df_time_agg, !(person == "XiaoyiLin" & coin == "0.50EUR"))
 df_time_agg <- subset(df_time_agg, !(person == "JasonNak" & coin == "0.50EUR"))
 
+print("Data reduced to:")
+sum(df_filtered$m)
+sum(df_filtered$m)/350757
+
 sampled_data <- df_time_agg %>%
   group_by(person, coin) %>%
   # Calculate the cumulative number of tosses
@@ -81,7 +108,6 @@ sampled_data_model <- sampled_data %>%
   group_by(person, coin) %>%
   summarize(m=sum(m), y=sum(y))
 
-df_time_agg$avg = df_time_agg$same_side / df_time_agg$N
 df_time_agg <- df_time_agg %>%
   group_by(person, coin) %>%
   mutate(total_tosses = cumsum(to - from + 1)) %>%
@@ -90,43 +116,86 @@ df_time_agg <- df_time_agg %>%
     "hundred_", floor((total_tosses - 1) / 100) + 1
   ))
 
+df_time_agg <- df_time_agg %>%
+  # Add the first set of rows
+  mutate(
+    m = N_start_heads_up,
+    y = heads_heads,
+    start = "heads"
+  ) %>%
+  bind_rows(
+    # Add the second set of rows
+    df_time_agg %>%
+      mutate(
+        m = N_start_tails_up,
+        y = N_start_tails_up - tails_heads,
+        start = "tails"
+      )
+  )
+
+df_time_agg <- df_time_agg %>%
+  select(y, m, person, coin, start, hundred_group)
+
+df_time_agg <- df_time_agg %>%
+  anti_join(to_remove, by = c("person", "coin"))
+
+#step(glmer(cbind(y,m-y)~1+person+coin+start+(1|hundred_group), data=df_time_agg, family=binomial), alpha.fixed=0.01, reduce.random=FALSE)
+
 #MIXED MODEL
-mixedIntercept <- lmer(avg~1 + (1|hundred_group), data=df_time_agg)
-mixedTotal <- lmer(avg~1 + person + coin + (1|hundred_group), data=df_time_agg)
-mixedTotalNested <- lmer(avg~1 + person/coin + (1|hundred_group), data=df_time_agg)
-mixedPerson <- lmer(avg~1 + person + (1|hundred_group), data=df_time_agg)
+#mixedIntercept <- glmer(avg~1 + (1|hundred_group), data=df_time_agg, family=binomial)
+#mixedPersonCoin <- glmer(avg~1 + person + coin + (1|hundred_group), data=df_time_agg, family=binomial)
+#mixedNested <- glmer(avg~1 + person/coin + (1|hundred_group), data=df_time_agg, family=binomial)
+#mixedPerson <- glmer(avg~1 + person + (1|hundred_group), data=df_time_agg, family=binomial)
+#mixedPersonStart <- glmer(avg~1 + person + start + (1|hundred_group), data=df_time_agg, family=binomial)
+#mixedTotal <- glmer(avg~1 + person + coin + start + (1|hundred_group), data=df_time_agg, family=binomial)
+#mixedTotalNested <- glmer(avg~1 + person/coin + start + (1|hundred_group), data=df_time_agg, family=binomial)
 
-anova(mixedIntercept, mixedPerson, mixedTotal, mixedTotalNested)
+#tab1 <- AIC(mixedIntercept, mixedPerson, mixedPersonCoin, mixedNested, mixedPersonStart, mixedTotal, mixedTotalNested)
+#tab2 <- AICc(mixedIntercept, mixedPerson, mixedPersonCoin, mixedNested, mixedPersonStart, mixedTotal, mixedTotalNested)
+#tab3 <- BIC(mixedIntercept, mixedPerson, mixedPersonCoin, mixedNested, mixedPersonStart, mixedTotal, mixedTotalNested)
+#tab_lr_selection <- cbind(tab1, tab2, tab3)
+#print(tab_lr_selection)
 
-print("AIC:")
-AIC(mixedIntercept)
-AIC(mixedPerson)
-AIC(mixedTotal)
-AIC(mixedTotalNested)
+#anova(mixedPerson, mixedPersonStart)
+#anova(mixedPerson, mixedNested)
 
-print("AICc:")
-AICc(mixedIntercept)
-AICc(mixedPerson)
-AICc(mixedTotal)
-AICc(mixedTotalNested)
+#Takes very long (hours)
+mixed_model <- glmer(cbind(y,m-y)~1+person+(person|hundred_group), data=df_time_agg, family=binomial, control = lme4::glmerControl(optimizer = "optimx", calc.derivs = FALSE, optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE)))
+mixed_modelCoin <- glmer(cbind(y,m-y)~1+person + coin +(person|hundred_group), data=df_time_agg, family=binomial, control = lme4::glmerControl(optimizer = "optimx", calc.derivs = FALSE, optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE)))
+mixed_modelStart <- glmer(cbind(y,m-y)~1+person + start + (person|hundred_group), data=df_time_agg, family=binomial, control = lme4::glmerControl(optimizer = "optimx", calc.derivs = FALSE, optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE)))
 
-print("BIC")
-BIC(mixedIntercept)
-BIC(mixedPerson)
-BIC(mixedTotal)
-BIC(mixedTotalNested)
+anova(mixed_model, mixed_modelCoin, test="LRT")
+anova(mixed_model, mixed_modelStart, test="LRT")
 
-mixed_model <- lmer(avg~(1|hundred_group), data=df_time_agg)
 summary(mixed_model)
+print("Model")
+AIC(mixed_model)
+AICc(mixed_model)
+BIC(mixed_model)
+print("ModelCoin")
+AIC(mixed_modelCoin)
+AICc(mixed_modelCoin)
+BIC(mixed_modelCoin)
+print("ModelStart")
+AIC(mixed_modelStart)
+AICc(mixed_modelStart)
+BIC(mixed_modelStart)
 
-plot(fitted(mixed_model), resid(mixed_model))
-plot(df_time_agg$same_side/df_time_agg$N, resid(mixed_model))
-plot(df_time_agg$same_side/df_time_agg$N, fitted(mixed_model))
+min(fixef(mixed_model)[-1])
+max(fixef(mixed_model)[-1])
+mean(fixef(mixed_model)[-1])
+sd(fixef(mixed_model)[-1])
+min(resid(mixed_model, scaled=TRUE))
+max(resid(mixed_model, scaled=TRUE))
 
-qqnorm(resid(mixed_model), main = "QQ-plot", xlab = "Quantiles", ylab = "Quantiles empiriques")
-qqline(resid(mixed_model), col = "red", lwd = 2)
+plot(fitted(mixed_model), resid(mixed_model, scaled=TRUE), main="Standardized residuals vs. fitted values", xlab="Fitted values", ylab="Standardized Residuals")
+plot(df_time_agg$y/df_time_agg$m, resid(mixed_model, scaled=TRUE), main="Standardized residuals vs. success probabilities", xlab="Success probabilities", ylab="Standardized Residuals")
+plot(df_time_agg$y/df_time_agg$m, fitted(mixed_model))
 
-residuals_df <- data.frame(Residuals = resid(mixed_model), Index = seq_along(resid(mixed_model)))
+qqnorm(resid(mixed_model, scaled=TRUE), main = "QQ-plot", xlab = "Quantiles", ylab = "Empirical Quantiles")
+qqline(resid(mixed_model, scaled=TRUE), col = "red", lwd = 2)
+
+residuals_df <- data.frame(Residuals = resid(mixed_model, scaled=TRUE), Index = seq_along(resid(mixed_model, scaled=TRUE)))
 ggplot(residuals_df, aes(x = Index, y = Residuals)) +
   geom_line() +
   labs(x = "Index", y = "Residuals") +
@@ -138,12 +207,17 @@ cooks_distance <- cooks.distance(mixed_model)
 plot(cooks_distance, main = "Cook's Distance", ylab = "Distance", type = "h", col = "blue")
 abline(h = 4/(length(cooks_distance)), col = "red", lty = 2) # rule of thumb 
 
+threshold <- 4 / nrow(df_time_agg)
+influential_points <- which(cooks_distance > threshold)
+influential_data <- df_time_agg[influential_points, ]
+print(influential_data)
+
 pred_probs<- predict(mixed_model, type = "response")
 print(mean(pred_probs))
 
-actual_classes <- ifelse(df_time_agg$same_side > (df_time_agg$N / 2), 1, 0)
+actual_classes <- ifelse(df_time_agg$y > df_time_agg$m/2 , 1, 0)
 roc_curve <- roc(actual_classes, pred_probs)
-plot(roc_curve, main = "Courbe ROC", col = "blue", lwd = 2)
+plot(roc_curve, main = "ROC curve", col = "blue", lwd = 2)
 auc(roc_curve)
 
 
@@ -152,15 +226,15 @@ pred_probs <- predict(mixed_model, type = "response")
 
 # Calculer les succès attendus (probabilité * nombre d'essais)
 # Ici, m est le nombre total d'essais pour chaque observation
-pred_successes <- pred_probs * df_time_agg$N
+pred_successes <- pred_probs * df_time_agg$m
 
 # Convertir en classes binaires : succès ou échec
 # Si le nombre de succès observé est supérieur à la moitié du nombre total d'essais, on peut dire que c'est un succès global
-pred_classes <- ifelse(pred_successes > (df_time_agg$N / 2), 1, 0)
+pred_classes <- ifelse(pred_successes > (df_time_agg$m / 2), 1, 0)
 
 # Convertir les valeurs réelles en classes binaires
 # Si y (le nombre de succès observé) est supérieur à la moitié du nombre total d'essais, c'est un succès global
-actual_classes <- ifelse(df_time_agg$same_side > (df_time_agg$N / 2), 1, 0)
+actual_classes <- ifelse(df_time_agg$y > (df_time_agg$m / 2), 1, 0)
 
 # Créer la confusion matrix en comparant les classes binaires observées et prédites
 confusion_matrix <- table(Predicted = pred_classes, Actual = actual_classes)
